83c69ef98747bdf0ae4f6027020433cf2c1854fb
Klaas, Wilfried <w.klaas@gmx.de>
2021-12-14 08:47:08 +0100

    implementing the gRPC server and a client, just for testing gRPC.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"time"
+
 	"github.com/willie68/cel-service/internal/logging"
 	log "github.com/willie68/cel-service/internal/logging"
+	"github.com/willie68/cel-service/pkg/protofiles"
+	"google.golang.org/grpc"
 )
+
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+func runEvaluate(client protofiles.EvalServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	celRequest := protofiles.CelRequest{
+		Context: map[string]*protofiles.ContextValue{
+			"data": {
+				Type: protofiles.ContextValue_map,
+				Vmap: map[string]*protofiles.ContextValue{
+					"value": {
+						Type:  1,
+						Value: []byte{0, 0, 0, 1},
+					},
+				},
+			},
+		},
+		Expression: "data.value == 1",
+	}
+
+	celResponse, err := client.Evaluate(ctx, &celRequest)
+	if err != nil {
+		log.Logger.Fatalf("%v.Evaluate(_) = _, %v: ", client, err)
+	}
+	log.Logger.Infof("res: %v", celResponse)
+}
 
 func init() {
 	initLogging()
@@ -11,6 +49,20 @@ func init() {
 
 func main() {
 	log.Logger.Info("starting")
+
+	flag.Parse()
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Logger.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := protofiles.NewEvalServiceClient(conn)
+
+	// evaluate
+	runEvaluate(client)
 
 	log.Logger.Info("finished")
 }
